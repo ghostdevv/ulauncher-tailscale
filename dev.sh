9138b3a66098f10b1c99d0c250be03c321156b2d
GHOST <ghostdevbusiness@gmail.com>
2025-06-16 01:47:50 +0100

    fix: make dev script cleanup correctly before run

--- dev.sh
+++ dev.sh
@@ -5,4 +5,6 @@ if [[ ! -e $EXTENSION_DIR ]]; then
     mkdir $EXTENSION_DIR;
 fi
 
-pnpm dlx chokidar-cli '**.{py,json,png}' --command "pkill -9 ulauncher && cp -r ./* $EXTENSION_DIR";
+pnpm dlx chokidar-cli \
+    '**.{py,json,png}' \
+    --command "rm -rf $EXTENSION_DIR && mkdir $EXTENSION_DIR && cp -r ./* $EXTENSION_DIR && pkill -9 ulauncher";
